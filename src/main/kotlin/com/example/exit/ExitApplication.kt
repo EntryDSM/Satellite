1a501b35e8647d1dd9330f850bc5da6eeea514f6
osangu <osangu012@gmail.com>
2022-11-29 10:38:14 +0900

    inital commit

---
+++ src/main/kotlin/com/example/exit/ExitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.exit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExitApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExitApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/exit/ExitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.exit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
