7388c35c731d5fc255b40180a1df922438de432b
osangu <osangu012@gmail.com>
2022-11-30 19:47:45 +0900

    (#1) add :: DetailService 추가

---
+++ src/main/kotlin/com/example/exit/global/security/auth/details/service/CompanyDetailService.kt
@@ -0,0 +1,7 @@
+package com.example.exit.global.security.auth.details.service
+
+import org.springframework.stereotype.Component
+
+@Component
+class CompanyDetailService {
+}
---
+++ src/main/kotlin/com/example/exit/global/security/auth/details/service/TeacherDetailService.kt
@@ -0,0 +1,8 @@
+package com.example.exit.global.security.auth.details.service
+
+import org.springframework.stereotype.Component
+
+@Component
+class TeacherDetailService {
+
+}
