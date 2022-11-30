91336870c628f8ba524f93f848b65c0d085fa6f7
osangu <osangu012@gmail.com>
2022-11-30 20:01:04 +0900

    (#1) add :: secutiryConfig

---
+++ src/main/kotlin/com/example/exit/global/security/SecurityConfig.kt
@@ -0,0 +1,33 @@
+package com.example.exit.global.security
+
+import com.example.exit.global.security.jwt.JwtTokenParser
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+internal class SecurityConfig(
+    private val jwtTokenParser: JwtTokenParser
+){
+    @Throws(Exception::class)
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .formLogin().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().cors()
+
+        http
+            .authorizeRequests()
+            .anyRequest().permitAll()
+
+        http
+            .apply(FilterConfig(jwtTokenParser))
+
+        return http.build()
+    }
+}
