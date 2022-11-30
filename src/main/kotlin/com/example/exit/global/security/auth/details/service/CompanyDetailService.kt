f71c05a020e1ba18a523ba8f65f3e010b74bdce2
osangu <osangu012@gmail.com>
2022-11-30 20:29:26 +0900

    (#1) update :: Service 상속 + TODO

--- src/main/kotlin/com/example/exit/global/security/auth/details/service/CompanyDetailService.kt
+++ src/main/kotlin/com/example/exit/global/security/auth/details/service/CompanyDetailService.kt
@@ -1,7 +1,16 @@
 package com.example.exit.global.security.auth.details.service
 
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 
 @Component
-class CompanyDetailService {
+class CompanyDetailService (
+    // private val companyRepository: CompanyRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+        TODO("Company Entity 만들고 진행")
+    }
+
 }
