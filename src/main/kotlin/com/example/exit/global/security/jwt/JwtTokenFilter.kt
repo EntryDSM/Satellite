a734c0873b891b1239b4177874023b5ea0dce919
osangu <osangu012@gmail.com>
2022-11-30 19:59:54 +0900

    (#1) add :: Filter

---
+++ src/main/kotlin/com/example/exit/global/security/jwt/JwtTokenFilter.kt
@@ -0,0 +1,22 @@
+package com.example.exit.global.security.jwt
+
+
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtTokenFilter(
+    private val jwtTokenParser: JwtTokenParser
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+
+        TODO("JWT_TOKEN_PARSER 만들고 진행")
+
+    }
+}
