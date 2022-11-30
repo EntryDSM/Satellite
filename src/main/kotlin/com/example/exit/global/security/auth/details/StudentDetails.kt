b466a85f0258a95df252e758511e5c39d2f31ad2
osangu <osangu012@gmail.com>
2022-11-30 19:47:12 +0900

    (#1) add :: details 추가

---
+++ src/main/kotlin/com/example/exit/global/security/auth/details/StudentDetails.kt
@@ -0,0 +1,28 @@
+package com.example.exit.global.security.auth.details
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class StudentDetails(
+
+) : UserDetails {
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        TODO("Student Entity 만들고 하기")
+    }
+
+    override fun getPassword(): String? =  null;
+
+    override fun getUsername(): String {
+        TODO("Student Entity 만들고 하기")
+    }
+
+    override fun isAccountNonExpired(): Boolean = true;
+
+    override fun isAccountNonLocked(): Boolean = true;
+
+    override fun isCredentialsNonExpired(): Boolean = true;
+
+    override fun isEnabled(): Boolean = true;
+
+}
