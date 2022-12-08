6ab51f8f8ee71ee7ec346a9f87f32e3c075142d9
osangu <osangu012@gmail.com>
2022-12-08 19:31:31 +0900

    (#5) settings :: properties 설정

---
+++ src/main/kotlin/com/example/exit/global/security/jwt/properties/SecurityProperties.kt
@@ -0,0 +1,17 @@
+package com.example.exit.global.security.jwt.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.util.Base64
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+data class SecurityProperties(
+    var secretKey: String,
+    val accessExp: Int,
+    val refreshExp: Int
+) {
+    init {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+    }
+}
