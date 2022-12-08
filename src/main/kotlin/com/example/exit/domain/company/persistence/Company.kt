31d65eb3322acff738953a5775a229eb779f6fc3
osangu <osangu012@gmail.com>
2022-12-08 13:14:49 +0900

    (#5) settings :: Company Entity & Repository

---
+++ src/main/kotlin/com/example/exit/domain/company/persistence/Company.kt
@@ -0,0 +1,29 @@
+package com.example.exit.domain.company.persistence
+
+import org.hibernate.validator.constraints.Length
+
+import com.example.exit.global.entity.BaseUUIDEntity
+import org.jetbrains.annotations.NotNull
+import javax.persistence.Entity
+import javax.persistence.Table
+
+@Entity
+@Table(name = "tbl_company")
+class Company(
+
+    @field: NotNull
+    val name: String,
+
+    @field: NotNull
+    @Length(min = 5)
+    val email: String,
+
+    @field: NotNull
+    @Length(max = 11)
+    val phoneNumber: String,
+
+    @field: NotNull
+    @Length(max = 60)
+    val password: String
+
+) : BaseUUIDEntity()
