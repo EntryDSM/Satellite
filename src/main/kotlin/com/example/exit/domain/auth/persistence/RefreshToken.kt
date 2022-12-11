be3f5cd921e6f8d2bc2e73e7ca0450d2cef96eb8
osangu <osangu012@gmail.com>
2022-12-11 22:58:55 +0900

    (#8) settings :: RefreshToken & Repository

---
+++ src/main/kotlin/com/example/exit/domain/auth/persistence/RefreshToken.kt
@@ -0,0 +1,23 @@
+package com.example.exit.domain.auth.persistence
+
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
+import java.util.UUID
+import javax.persistence.Id
+import javax.validation.constraints.NotBlank
+
+@RedisHash
+class RefreshToken(
+
+    @Id
+    @Indexed
+    val userId: UUID,
+
+    @NotBlank
+    val token: String,
+
+    @TimeToLive
+    val timeToLive: Long
+
+)
