2ecddecb525d37a78c9c9bf72f52c6c15ed9db6e
junseo <ljchan71@gmail.com>
2022-12-13 19:05:26 +0900

    (#10) add :: 전화번호 인증코드 발송 usecase

---
+++ src/main/kotlin/com/example/exit/domain/auth/usecase/SendPhoneNumberVerificationCodeUseCase.kt
@@ -0,0 +1,69 @@
+package com.example.exit.domain.auth.usecase
+
+import com.example.exit.domain.auth.exception.AlreadyVerifiedException
+import com.example.exit.domain.auth.exception.TooManySendVerificationException
+import com.example.exit.domain.auth.persistence.PhoneNumberVerificationCode
+import com.example.exit.domain.auth.persistence.properties.PhoneNumberVerificationCodeProperties
+import com.example.exit.domain.auth.persistence.repository.PhoneNumberVerificationCodeRepository
+import com.example.exit.domain.auth.presentation.dto.response.SendPhoneNumberCodeResponse
+import com.example.exit.domain.auth.util.GenerateRandomCodeUtil
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class SendPhoneNumberVerificationCodeUseCase(
+    private val phoneNumberVerificationCodeRepository: PhoneNumberVerificationCodeRepository,
+    private val properties: PhoneNumberVerificationCodeProperties
+) {
+
+    fun execute(phoneNumber: String): SendPhoneNumberCodeResponse {
+
+        val phoneNumberVerificationCode = phoneNumberVerificationCodeRepository.findByIdOrNull(phoneNumber)
+
+        val code = GenerateRandomCodeUtil.randomNumber(properties.codeLength)
+
+        val countOfSend = phoneNumberVerificationCode?.countOfSend ?: 0
+
+        if (countOfSend >= properties.limitCountOfSend) {
+            throw TooManySendVerificationException
+        }
+
+        if ((phoneNumberVerificationCode?.timeToLive ?: false) == true) {
+            throw AlreadyVerifiedException
+        }
+
+        phoneNumberVerificationCodeRepository.save(
+            PhoneNumberVerificationCode(
+                phoneNumber = phoneNumber,
+                code = code,
+                timeToLive = properties.baseTimeToLive,
+                countOfSend = countOfSend,
+                isVerified = false
+            )
+        )
+
+        sendAuthCode(phoneNumber, code)
+
+        return SendPhoneNumberCodeResponse(code)
+    }
+
+    fun sendAuthCode(phoneNumber: String, authCode: String?) {
+        val message = Message(properties.key, properties.secret)
+
+        val params: HashMap<String, String> = HashMap()
+        params["to"] = phoneNumber
+        params["from"] = properties.phoneNumber
+        params["type"] = "SMS"
+        params["text"] = "인증번호 " + authCode + "를 입력하세요."
+        params["app_version"] = "app 1.0"
+
+        try {
+            message.send(params)
+        } catch (e: CoolsmsException) {
+            e.stackTrace
+        }
+    }
+}
+
