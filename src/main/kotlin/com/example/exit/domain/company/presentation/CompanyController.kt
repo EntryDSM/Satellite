5a339ee215937e6550661c0c8e9819abe21e4dca
junseo <ljchan71@gmail.com>
2022-12-13 18:50:42 +0900

    (#10) add :: CompanyController

---
+++ src/main/kotlin/com/example/exit/domain/company/presentation/CompanyController.kt
@@ -0,0 +1,43 @@
+package com.example.exit.domain.company.presentation
+
+import com.example.exit.domain.company.presentation.dto.request.SignUpRequestDto
+import com.example.exit.domain.company.usecase.AllowStandByCompanyUseCase
+import com.example.exit.domain.company.usecase.CompanySignUpUseCase
+import com.example.exit.domain.company.usecase.RejectStandByCompanyUseCase
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+import javax.validation.Valid
+
+@RequestMapping("/company")
+@RestController
+class CompanyController(
+    private val companySignUpUseCase: CompanySignUpUseCase,
+    private val allowStandByCompanyUseCase: AllowStandByCompanyUseCase,
+    private val rejectStandByCompanyUseCase: RejectStandByCompanyUseCase
+) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/sign-up")
+    fun singUp(@RequestBody @Valid request: SignUpRequestDto) {
+        companySignUpUseCase.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/stand-by/{standby-company-id}")
+    fun allowStandByCompany(@PathVariable("standby-company-id") standbyCompanyId: UUID) {
+        allowStandByCompanyUseCase.execute(standbyCompanyId)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/stand-by/{standby-company-id}")
+    fun rejectStandByCompany(@PathVariable("standby-company-id") standbyCompanyId: UUID) {
+        rejectStandByCompanyUseCase.execute(standbyCompanyId)
+    }
+}
