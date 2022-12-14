0139c77deedce01251a723ff1f01b78de9d51e2b
osangu <osangu012@gmail.com>
2022-12-14 16:48:04 +0900

    (#7) update :: 줄바꿈

--- src/main/kotlin/com/example/exit/domain/student/presentation/StudentController.kt
+++ src/main/kotlin/com/example/exit/domain/student/presentation/StudentController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/student")
 @RestController
-
 class StudentController(
     private val updateProfileUseCase: StudentUpdateProfileUseCase
 ) {
