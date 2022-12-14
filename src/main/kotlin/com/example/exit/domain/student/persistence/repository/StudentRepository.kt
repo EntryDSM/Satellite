6368c9efe058659d1f446680bcce2ecd24521148
osangu <osangu012@gmail.com>
2022-12-14 10:54:43 +0900

    (#7) feat :: findByEmail

--- src/main/kotlin/com/example/exit/domain/student/persistence/repository/StudentRepository.kt
+++ src/main/kotlin/com/example/exit/domain/student/persistence/repository/StudentRepository.kt
@@ -3,8 +3,11 @@ package com.example.exit.domain.student.persistence.repository
 import com.example.exit.domain.student.persistence.Student
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
 interface StudentRepository: CrudRepository<Student, UUID> {
+
+    fun findByEmail(email: String): Student?
+
 }
