b6712b5991d521d9e1e2fb156f9af90ee9f6f468
osangu <osangu012@gmail.com>
2022-12-14 16:49:00 +0900

    (#7) update :: google url 묶기

--- src/main/kotlin/com/example/exit/domain/student/presentation/StudentGoogleOauthController.kt
+++ src/main/kotlin/com/example/exit/domain/student/presentation/StudentGoogleOauthController.kt
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/student")
+@RequestMapping("/student/google")
 @RestController
 class StudentGoogleOauthController(
     private val studentAuthService: StudentGoogleOauthUseCase
 ) {
 
-    @GetMapping("/google/link")
+    @GetMapping("/link")
     fun getGoogleClientId(): GoogleLoginLinkResponse {
         return studentAuthService.getLink()
     }
 
-    @GetMapping("/google/redirect")
+    @GetMapping("/redirect")
     fun studentSingUpOrIn(@RequestParam("code") code: String): TokenResponse {         // Post로 변경하기 TODO
         return studentAuthService.signUpOrIn(code)
     }
