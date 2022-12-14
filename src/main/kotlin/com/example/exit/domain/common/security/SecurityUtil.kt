8bfa9fa7241f8d12ec745bdb9432590724adc7d3
osangu <osangu012@gmail.com>
2022-12-14 11:28:50 +0900

    (#7) :: SecurityUtil object로 타입 변경

--- src/main/kotlin/com/example/exit/domain/common/security/SecurityUtil.kt
+++ src/main/kotlin/com/example/exit/domain/common/security/SecurityUtil.kt
@@ -4,8 +4,8 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import java.util.UUID
 
-@Component
-class SecurityUtil {
+
+object SecurityUtil {
     fun getCurrentUserId(): UUID {
         return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
     }
