b3d31d68c33339077462970daf9c7d74c3a7d1c7
osangu <osangu012@gmail.com>
2022-12-14 11:02:20 +0900

    (#7) add :: 학생 정보 갱신 Request DTO

---
+++ src/main/kotlin/com/example/exit/domain/student/presentation/dto/request/StudentUpdateRequest.kt
@@ -0,0 +1,10 @@
+package com.example.exit.domain.student.presentation.dto.request
+
+data class StudentUpdateRequest (
+    val name: String,
+    val profileImagePath: String,
+    val grade: String,
+    val classNum: String,
+    val number: String,
+    val major: String
+)
