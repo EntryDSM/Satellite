b94f1a9de63c0719ba256b16c057997ca1afcb34
rlaisqls <rlaisqls@gmail.com>
2022-12-30 13:27:57 +0900

    (#13) create :: ProjectElement

---
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/document/persistence/element/ProjectElement.kt
@@ -0,0 +1,16 @@
+package kr.hs.entrydsm.exit.domain.document.persistence.element
+
+import java.util.*
+
+
+class ProjectElement (
+
+    val name: String,
+    val representImagePath: String,
+    val startDate: Date,
+    val endDate: Date,
+    val skillSet: List<String>,
+    val description: String,
+    val url: String?
+
+)
