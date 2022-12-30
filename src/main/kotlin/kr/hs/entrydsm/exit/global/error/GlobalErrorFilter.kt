7c0fadaab1d494d53339034ba54e2d941c9d472b
rlaisqls <rlaisqls@gmail.com>
2022-12-30 18:20:47 +0900

    (#18) update :: e.cause -> e

--- src/main/kotlin/kr/hs/entrydsm/exit/global/error/GlobalErrorFilter.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/error/GlobalErrorFilter.kt
@@ -1,9 +1,8 @@
 package kr.hs.entrydsm.exit.global.error
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import kr.hs.entrydsm.exit.domain.common.error.DomainCustomException
 import kr.hs.entrydsm.exit.global.error.custom.CustomErrorProperty
-import kr.hs.entrydsm.exit.global.error.custom.GlobalCustomException
+import kr.hs.entrydsm.exit.global.error.custom.CustomException
 import kr.hs.entrydsm.exit.global.error.response.DefaultErrorResponse
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
@@ -22,9 +21,10 @@ class GlobalErrorFilter(
         try {
             filterChain.doFilter(request, response)
         } catch (e: Exception) {
-            when (e.cause) {
-                is DomainCustomException -> sendErrorMessage(response, (e.cause as DomainCustomException).errorProperty)
-                is GlobalCustomException -> sendErrorMessage(response, (e.cause as GlobalCustomException).errorProperty)
+            when (e) {
+                is CustomException -> {
+                    sendErrorMessage(response, e.errorProperty)
+                }
                 else -> {
                     e.printStackTrace()
                 }
@@ -33,9 +33,11 @@ class GlobalErrorFilter(
     }
 
     private fun sendErrorMessage(response: HttpServletResponse, errorProperty: CustomErrorProperty) {
+
         response.status = errorProperty.status()
         response.contentType = "application/json"
         response.characterEncoding = "UTF-8"
+
         response.writer.write(
             objectMapper.writeValueAsString(
                 DefaultErrorResponse(
