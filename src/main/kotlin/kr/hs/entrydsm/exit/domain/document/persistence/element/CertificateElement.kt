571bd80130566bbd25926e784aeeaa1b712c02e8
rlaisqls <rlaisqls@gmail.com>
2022-12-30 13:27:39 +0900

    (#13) create :: CertificateElement

---
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/document/persistence/element/CertificateElement.kt
@@ -0,0 +1,11 @@
+package kr.hs.entrydsm.exit.domain.document.persistence.element
+
+import java.util.*
+
+class CertificateElement(
+
+    val name: String,
+    val issuingInstitution: String,
+    val date: Date
+
+)
