ea86dabfc10a67395007aa6e7f803a9e44b16651
rlaisqls <rlaisqls@gmail.com>
2023-01-02 23:36:56 +0900

    (#14) update :: ConstraintViolationException handling

--- src/main/kotlin/kr/hs/entrydsm/exit/global/error/GlobalErrorHandler.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/error/GlobalErrorHandler.kt
@@ -2,11 +2,15 @@ package kr.hs.entrydsm.exit.global.error
 
 import kr.hs.entrydsm.exit.global.error.response.BindErrorResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.validation.ConstraintViolation
+import javax.validation.ConstraintViolationException
+
 
 @RestControllerAdvice
 class GlobalErrorHandler {
@@ -25,5 +29,21 @@ class GlobalErrorHandler {
             status = HttpStatus.BAD_REQUEST.value(),
             fieldError = listOf(errorMap)
         )
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    protected fun handleConstraintViolation(e: ConstraintViolationException): ResponseEntity<Map<String, Any>>? {
+
+        val errorMap: MutableMap<String, Any> = HashMap()
+        val errors: MutableList<String> = ArrayList()
+        for (violation: ConstraintViolation<*> in e.constraintViolations) {
+            errors.add(
+                violation.rootBeanClass.name + " " +
+                        violation.propertyPath + ": " + violation.message
+            )
+        }
+        errorMap["errors"] = errors
+        errorMap["message"] = e.message ?: ""
+        return ResponseEntity(errorMap, HttpStatus.BAD_REQUEST)
     }
 }
