b3ff8616314d311a100f912525d5db715c6a6770
rlaisqls <rlaisqls@gmail.com>
2023-01-02 22:48:39 +0900

    (#21) update :: #findByNameContaining

--- src/main/kotlin/kr/hs/entrydsm/exit/domain/major/persistence/repository/MajorRepository.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/major/persistence/repository/MajorRepository.kt
@@ -4,4 +4,6 @@ import kr.hs.entrydsm.exit.domain.major.persistence.Major
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
-interface MajorRepository: CrudRepository<Major, UUID>
+interface MajorRepository: CrudRepository<Major, UUID> {
+    fun findByNameContaining(name: String): List<Major>
+}
