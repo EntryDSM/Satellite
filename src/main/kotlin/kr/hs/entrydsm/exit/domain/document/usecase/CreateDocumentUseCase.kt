91546a165bb9ffccfd8e63a5449a4ba905c47bdd
rlaisqls <rlaisqls@gmail.com>
2023-01-03 11:36:24 +0900

    (#24) update :: getCurrentStudent

--- src/main/kotlin/kr/hs/entrydsm/exit/domain/document/usecase/CreateDocumentUseCase.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/document/usecase/CreateDocumentUseCase.kt
@@ -1,22 +1,19 @@
 package kr.hs.entrydsm.exit.domain.document.usecase
 
-import kr.hs.entrydsm.exit.domain.document.persistence.enums.Status
+import kr.hs.entrydsm.exit.domain.common.security.SecurityUtil
 import kr.hs.entrydsm.exit.domain.document.exception.DocumentAlreadyExistException
 import kr.hs.entrydsm.exit.domain.document.exception.MajorNotFoundException
 import kr.hs.entrydsm.exit.domain.document.persistence.Document
 import kr.hs.entrydsm.exit.domain.document.persistence.element.WriterInfoElement
+import kr.hs.entrydsm.exit.domain.document.persistence.enums.Status
 import kr.hs.entrydsm.exit.domain.document.persistence.repository.DocumentRepository
 import kr.hs.entrydsm.exit.domain.document.presentation.dto.request.CreateDocumentRequest
 import kr.hs.entrydsm.exit.domain.document.presentation.dto.response.CreateDocumentResponse
 import kr.hs.entrydsm.exit.domain.major.persistence.repository.MajorRepository
-import kr.hs.entrydsm.exit.domain.student.exception.StudentNotFoundException
 import kr.hs.entrydsm.exit.domain.student.persistence.Student
 import kr.hs.entrydsm.exit.domain.student.persistence.repository.StudentRepository
-import kr.hs.entrydsm.exit.global.security.auth.details.StudentDetails
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
-import java.util.*
 import javax.transaction.Transactional
 
 @Service
@@ -28,8 +25,7 @@ class CreateDocumentUseCase(
     @Transactional
     fun execute(request: CreateDocumentRequest): CreateDocumentResponse {
 
-        val studentId = (SecurityContextHolder.getContext().authentication.principal as StudentDetails).username
-        val student = studentRepository.findByIdOrNull(UUID.fromString(studentId)) ?: throw StudentNotFoundException
+        val student = SecurityUtil.getCurrentStudent()
 
         if(writerIsExist(student)){
             throw DocumentAlreadyExistException
