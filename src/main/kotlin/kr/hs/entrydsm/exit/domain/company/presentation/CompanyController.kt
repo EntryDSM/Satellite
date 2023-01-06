e8d13063320d4949aa0123efa0b52e9f16367f51
rlaisqls <rlaisqls@gmail.com>
2023-01-06 09:47:10 +0900

    (#24) chore :: signup

--- src/main/kotlin/kr/hs/entrydsm/exit/domain/company/presentation/CompanyController.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/company/presentation/CompanyController.kt
@@ -5,14 +5,8 @@ import kr.hs.entrydsm.exit.domain.company.usecase.AllowStandbyCompanyUseCase
 import kr.hs.entrydsm.exit.domain.company.usecase.CompanySignUpUseCase
 import kr.hs.entrydsm.exit.domain.company.usecase.RejectStandbyCompanyUseCase
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
+import org.springframework.web.bind.annotation.*
+import java.util.*
 import javax.validation.Valid
 
 @RequestMapping("/company")
@@ -25,7 +19,7 @@ class CompanyController(
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sign-up")
-    fun singUp(@RequestBody @Valid request: CompanySignUpRequest) {
+    fun signUp(@RequestBody @Valid request: CompanySignUpRequest) {
         companySignUpUseCase.execute(request)
     }
 
