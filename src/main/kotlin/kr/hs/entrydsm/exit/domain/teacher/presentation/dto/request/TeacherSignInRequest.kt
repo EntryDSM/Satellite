a6018046e922ad83856fe6e3638cfdb8a850bccc
rlaisqls <rlaisqls@gmail.com>
2023-01-07 15:51:51 +0900

    (#20) chore :: Request validation

--- src/main/kotlin/kr/hs/entrydsm/exit/domain/teacher/presentation/dto/request/TeacherSignInRequest.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/teacher/presentation/dto/request/TeacherSignInRequest.kt
@@ -1,7 +1,13 @@
 package kr.hs.entrydsm.exit.domain.teacher.presentation.dto.request
 
+import javax.validation.constraints.NotNull
+
+
 data class TeacherSignInRequest(
+
+    @field:NotNull
     val accountId: String,
 
+    @field:NotNull
     val password: String
 )
