56add855f3ec619ffd33ac8390788a7c0647baa4
rlaisqls <rlaisqls@gmail.com>
2023-01-08 17:16:46 +0900

    (#31) chore :: Exception명 Global 접두사 삭제

--- src/main/kotlin/kr/hs/entrydsm/exit/global/exception/jwt/UnexpectedTokenException.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/exception/jwt/UnexpectedTokenException.kt
@@ -3,6 +3,6 @@ package kr.hs.entrydsm.exit.global.exception.jwt
 import kr.hs.entrydsm.exit.global.error.GlobalErrorCode
 import kr.hs.entrydsm.exit.global.error.custom.GlobalCustomException
 
-object GlobalExpiredTokenException: GlobalCustomException(
-    GlobalErrorCode.EXPIRED_JWT
+object UnexpectedTokenException: GlobalCustomException(
+    GlobalErrorCode.UNEXPECTED_JWT
 )
