bcda566592412649394bd5c6194f4c5239a1c804
rlaisqls <rlaisqls@gmail.com>
2023-01-09 22:00:24 +0900

    (#36) create :: SubmitMyDocumentUseCase

---
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/document/usecase/SubmitMyDocumentUseCase.kt
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.exit.domain.document.usecase
+
+import kr.hs.entrydsm.exit.domain.common.annotation.UseCase
+import kr.hs.entrydsm.exit.domain.document.exception.DocumentNotFoundException
+import kr.hs.entrydsm.exit.domain.document.exception.IllegalStatusException
+import kr.hs.entrydsm.exit.domain.document.persistence.enums.Status
+import kr.hs.entrydsm.exit.domain.document.persistence.repository.DocumentRepository
+import kr.hs.entrydsm.exit.global.security.SecurityUtil
+
+@UseCase
+class SubmitMyDocumentUseCase(
+    private val documentRepository: DocumentRepository
+) {
+
+    fun execute() {
+
+        val student = SecurityUtil.getCurrentStudent()
+        val document = documentRepository.findByWriterStudentId(student.id) ?: throw DocumentNotFoundException
+
+        if(document.status != Status.CREATED) {
+            throw IllegalStatusException
+        }
+
+        documentRepository.save(
+            document.updateStatus(Status.SUBMITTED)
+        )
+    }
+}
