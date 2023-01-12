afd917e0e250f7935b83f7dd038c8e0cb8733386
rlaisqls <rlaisqls@gmail.com>
2023-01-12 21:02:32 +0900

    (#38) setting :: querydsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
 
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
@@ -41,29 +42,38 @@ dependencies {
     // validation
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    // Fegin Client
-    implementation("io.github.openfeign:feign-httpclient:11.9.1")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
-
-    // cool sms
-    implementation("net.nurigo:javaSDK:2.2")
-    
-    //AWS
-    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
-
     // DB
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
 
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb:2.6.3")
-    runtimeOnly("org.mongodb:mongodb-driver-sync:4.8.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-mongodb:5.0.0")
+    api("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0")
+
+    // Fegin Client
+    implementation("io.github.openfeign:feign-httpclient:11.9.1")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
+
+    // AWS
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
+
+    // cool sms
+    implementation("net.nurigo:javaSDK:2.2")
 
     // test
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
-    testImplementation("io.mockk:mockk:1.13.2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.4.3")
+    testImplementation("io.kotest:kotest-extensions-spring:4.4.3")
+
+    testImplementation("io.mockk:mockk:1.13.2")
 }
 
 allOpen {
@@ -71,6 +81,18 @@ allOpen {
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
     annotation("org.springframework.data.mongodb.core.mapping.Document")
+}
+
+kapt {
+    annotationProcessor("org.springframework.data.mongodb.repository.support.MongoAnnotationProcessor")
+    annotationProcessor("com.querydsl.apt.jpa.JPAAnnotationProcessor")
+}
+
+querydsl {
+    springDataMongo = true
+    jpa = true
+    library = "com.querydsl:querydsl-apt:5.0.0"
+    querydslSourcesDir = "$projectDir/build/generated"
 }
 
 tasks.withType<KotlinCompile> {
