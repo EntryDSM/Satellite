8f759017a2619d32eff46c5c3c816d936c83bac3
rlaisqls <rlaisqls@gmail.com>
2023-01-26 13:30:10 +0900

    (#42) move :: feignClient thirdParty로

---
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/thirdparty/oauth/properties/GoogleOauthProperties.kt
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.exit.global.thirdparty.oauth.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+
+@ConfigurationProperties(prefix = "auth")
+@ConstructorBinding
+data class GoogleOauthProperties(
+    val clientId: String,
+    val secretKey: String,
+    val baseUrl: String,
+    val redirectUrl: String
+)
