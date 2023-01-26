3cf15457b1bb87af9012065c2613daaea0f1710c
rlaisqls <rlaisqls@gmail.com>
2023-01-26 16:01:05 +0900

    (#44) update :: bean type 지정

--- src/main/kotlin/kr/hs/entrydsm/exit/global/security/SecurityConfig.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/security/SecurityConfig.kt
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -87,5 +88,5 @@ internal class SecurityConfig(
     }
 
     @Bean
-    protected fun passwordEncoder() = BCryptPasswordEncoder()
+    protected fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
