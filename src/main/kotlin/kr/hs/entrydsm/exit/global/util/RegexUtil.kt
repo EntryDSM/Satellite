28dcf695eb04148e9bcf068b34dcdc181ba72f59
rlaisqls <rlaisqls@gmail.com>
2023-01-26 13:33:28 +0900

    (#42) refactor :: 가독성

--- src/main/kotlin/kr/hs/entrydsm/exit/global/util/RegexUtil.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/global/util/RegexUtil.kt
@@ -6,6 +6,7 @@ object RegexUtil {
     const val EMAIL_EXP = "^[a-zA-Z0-9.]+${schoolEmail}$"
 
     const val PASSWORD_EXP = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+\$).{8,30}"
+
     const val NUMBER_EXP = "^[0-9]+$"
 
 }
