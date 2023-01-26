7396f878eac277a5b4bc735de72403795195e5de
rlaisqls <rlaisqls@gmail.com>
2023-01-26 13:32:52 +0900

    (#42) create :: annotation config 분리

--- src/main/kotlin/kr/hs/entrydsm/exit/ExitApplication.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/ExitApplication.kt
@@ -3,10 +3,7 @@ package kr.hs.entrydsm.exit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 
-@ConfigurationPropertiesScan
-@EnableFeignClients
 @SpringBootApplication
 class ExitApplication
 
