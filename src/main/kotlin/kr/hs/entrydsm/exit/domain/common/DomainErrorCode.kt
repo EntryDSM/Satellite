b9fa71fe79c9b832224af169655ab3191e4044d2
rlaisqls <rlaisqls@gmail.com>
2023-02-08 19:18:34 +0900

    (#50) create :: companyException

--- src/main/kotlin/kr/hs/entrydsm/exit/domain/common/DomainErrorCode.kt
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/common/DomainErrorCode.kt
@@ -13,17 +13,18 @@ enum class DomainErrorCode(
 
     VERIFICATION_CODE_MISMATCHED(400, "AUTH-400-1", "Verification code mismatched"),
     NOT_VERIFIED(400, "AUTH-400-2", "Is not verified phone number"),
-    PHONE_NUMBER_VERIFICATION_CODE_NOT_FOUND(404, "AUTH-404-1", "Phone number verification code not found"),
+    VERIFICATION_CODE_NOT_FOUND(404, "AUTH-404-1", "Phone number verification code not found"),
     ALREADY_VERIFIED(409, "AUTH-409-1", "Is already verified"),
     TOO_MANY_SEND_VERIFICATION_CODE(429, "AUTH-429-1", "Too many send verification code / Please confirm later"),
 
-    STUDENT_NOT_FOUND(404, "STUDENT-404-1", "Student Not Found"),
-    STUDENT_ALREADY_EXIST(404, "STUDENT-404-1", "Student Already Exist"),
+    STUDENT_NOT_FOUND(404, "STUDENT-404-1", "Student not found"),
+    STUDENT_ALREADY_EXIST(409, "STUDENT-409-1", "Student already exist"),
 
     TEACHER_NOT_FOUND(404, "TEACHER-404-1", "Teacher not found"),
 
     COMPANY_NOT_FOUND(404, "COMPANY-404-1", "Company not found"),
     STANDBY_COMPANY_NOT_FOUND(404, "COMPANY-404-2", "Stand by company not found"),
+    COMPANY_INIT_REQUIRED(422, "COMPANY-422-1", "Company init required"),
 
     MAJOR_NOT_FOUND(404, "MAJOR-404-1", "Major not found"),
 
