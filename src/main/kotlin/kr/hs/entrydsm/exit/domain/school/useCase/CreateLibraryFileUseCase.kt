1fe613f98faaf8f66fe6fa5f25b14ae46b878c14
rlaisqls <rlaisqls@gmail.com>
2023-03-19 15:52:28 +0900

    feat :: LibraryDocument

---
+++ src/main/kotlin/kr/hs/entrydsm/exit/domain/school/useCase/CreateLibraryFileUseCase.kt
@@ -0,0 +1,54 @@
+package kr.hs.entrydsm.exit.domain.school.useCase
+
+import kr.hs.entrydsm.exit.domain.document.persistence.repository.DocumentRepository
+import kr.hs.entrydsm.exit.domain.school.facade.SchoolYearFacade
+import kr.hs.entrydsm.exit.domain.school.persistence.AccessRight
+import kr.hs.entrydsm.exit.domain.school.persistence.LibraryDocument
+import kr.hs.entrydsm.exit.domain.school.persistence.repository.LibraryDocumentRepository
+import kr.hs.entrydsm.exit.domain.school.presentation.dto.CreateLibraryFileResponse
+import kr.hs.entrydsm.exit.domain.school.properties.SchoolYearProperties
+import kr.hs.entrydsm.exit.global.exception.ForbiddenException
+import kr.hs.entrydsm.exit.global.thirdparty.aws.s3.AwsS3Adapter
+import kr.hs.entrydsm.exit.global.thirdparty.pdf.PdfAdapter
+import kr.hs.entrydsm.exit.global.util.FileUtil.toFileDateFormat
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
+import java.io.File
+import java.time.LocalDateTime
+
+@Component
+class CreateLibraryFileUseCase(
+    private val passwordEncoder: PasswordEncoder,
+    private val schoolYearProperties: SchoolYearProperties,
+    private val schoolYearFacade: SchoolYearFacade,
+    private val pdfAdapter: PdfAdapter,
+    private val documentRepository: DocumentRepository,
+    private val awsS3Adapter: AwsS3Adapter,
+    private val libraryDocumentRepository: LibraryDocumentRepository
+) {
+    fun execute(grade: Int, secret: String): CreateLibraryFileResponse {
+
+        if (passwordEncoder.matches(secret, schoolYearProperties.secret)) {
+            throw ForbiddenException
+        }
+        val year = schoolYearFacade.getSchoolYear()
+
+        val bytes = pdfAdapter.generateGradeLibraryDocument(
+            documentRepository.findByYearAndWriterGrade(year, grade.toString())
+        )
+        val filePath = awsS3Adapter.savePdf(
+            File("${year}_${grade}_${LocalDateTime.now().toFileDateFormat()}")
+                .apply { writeBytes(bytes) }
+        )
+
+        val libraryDocument = libraryDocumentRepository.save(
+            LibraryDocument(
+                year = year,
+                grade = grade,
+                filePath = filePath,
+                accessRight = AccessRight.PRIVATE
+            )
+        )
+        return CreateLibraryFileResponse(libraryDocument.id)
+    }
+}
