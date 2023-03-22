9cecc64b06c9e15137ea0504b8daa45b30c940c8
rlaisqls <rlaisqls@gmail.com>
2023-03-22 18:41:53 +0900

    feat :: UpdateLibraryDocumentAccessRightUseCase

---
+++ src/main/kotlin/kr/hs/entrydsm/repo/domain/library/usecase/UpdateLibraryDocumentAccessRightUseCase.kt
@@ -0,0 +1,23 @@
+package kr.hs.entrydsm.repo.domain.library.usecase
+
+import kr.hs.entrydsm.repo.domain.common.annotation.ReadOnlyUseCase
+import kr.hs.entrydsm.repo.domain.library.exception.LibraryDocumentNotFoundException
+import kr.hs.entrydsm.repo.domain.library.persistence.AccessRight
+import kr.hs.entrydsm.repo.domain.library.persistence.repository.LibraryDocumentRepository
+import org.springframework.data.repository.findByIdOrNull
+import java.util.UUID
+
+
+@ReadOnlyUseCase
+class UpdateLibraryDocumentAccessRightUseCase(
+    private val libraryDocumentRepository: LibraryDocumentRepository
+) {
+    fun execute(libraryDocumentId: UUID, accessRight: AccessRight) {
+        val libraryDocument =
+            libraryDocumentRepository.findByIdOrNull(libraryDocumentId) ?: throw LibraryDocumentNotFoundException
+
+        libraryDocumentRepository.save(
+            libraryDocument.updateLibraryDocumentRight(accessRight)
+        )
+    }
+}
