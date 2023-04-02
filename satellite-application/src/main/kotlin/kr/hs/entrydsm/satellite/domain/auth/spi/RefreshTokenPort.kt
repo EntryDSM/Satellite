59a9967696c07651edc079c372eb7bdcf4d7627d
rlaisqls <rlaisqls@gmail.com>
2023-04-02 22:09:12 +0900

    feat :: save 메서드 추가

--- satellite-application/src/main/kotlin/kr/hs/entrydsm/satellite/domain/auth/spi/RefreshTokenPort.kt
+++ satellite-application/src/main/kotlin/kr/hs/entrydsm/satellite/domain/auth/spi/RefreshTokenPort.kt
@@ -3,5 +3,6 @@ package kr.hs.entrydsm.satellite.domain.auth.spi
 import kr.hs.entrydsm.satellite.domain.auth.domain.RefreshToken
 
 interface RefreshTokenPort {
-   fun queryByToken(token: String): RefreshToken?
+    fun save(refreshToken: RefreshToken): RefreshToken
+    fun queryByToken(token: String): RefreshToken?
 }
