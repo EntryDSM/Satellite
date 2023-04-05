32a489031f765aafdfb950fab82d6cb988f7e94c
rlaisqls <rlaisqls@gmail.com>
2023-04-05 22:25:37 +0900

    feat :: library api 추가

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityConfig.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityConfig.kt
@@ -2,16 +2,19 @@ package kr.hs.entrydsm.satellite.global.security
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import kr.hs.entrydsm.satellite.domain.auth.domain.Authority
+import kr.hs.entrydsm.satellite.global.config.FilterConfig
 import kr.hs.entrydsm.satellite.global.security.token.JwtParser
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
+@EnableWebSecurity(debug = true)
 @Configuration
 internal class SecurityConfig(
     private val jwtParser: JwtParser,
@@ -37,23 +40,14 @@ internal class SecurityConfig(
             .authorizeRequests()
             // AUTH
             .antMatchers("/auth/**").permitAll()
+
             // FILE
             .antMatchers(HttpMethod.POST, "/file/image").permitAll()
-            .antMatchers(HttpMethod.GET, "/file").permitAll()
+
             // STUDENT
-            .antMatchers(HttpMethod.GET, "/student/google/link").permitAll()
-            .antMatchers(HttpMethod.GET, "/student/google/sign").permitAll()
             .antMatchers(HttpMethod.POST, "/student").permitAll()
             .antMatchers(HttpMethod.GET, "/student").hasAuthority(TEACHER)
-            .antMatchers(HttpMethod.POST, "/student/sign/test").permitAll()
-            // COMPANY
-            .antMatchers(HttpMethod.POST, "/company/auth").permitAll()
-            .antMatchers(HttpMethod.PATCH, "/company/password").permitAll()
-            .antMatchers(HttpMethod.GET, "/company").hasAuthority(TEACHER)
-            .antMatchers(HttpMethod.POST, "/company/sing-up").permitAll()
-            .antMatchers(HttpMethod.POST, "/company/standby/{standby-company-id}").hasAuthority(TEACHER)
-            .antMatchers(HttpMethod.DELETE, "/company/standby/{standby-company-id}").hasAuthority(TEACHER)
-            .antMatchers(HttpMethod.GET, "/company/standby").hasAuthority(TEACHER)
+
             // DOCUMENT
             .antMatchers(HttpMethod.POST, "/document").hasAuthority(STUDENT)
             .antMatchers(HttpMethod.PATCH, "/document/writer-info").hasAuthority(STUDENT)
@@ -71,10 +65,17 @@ internal class SecurityConfig(
             .antMatchers(HttpMethod.GET, "/document/shared").hasAnyAuthority(STUDENT, TEACHER)
             .antMatchers(HttpMethod.GET, "/document/{document-id}/paging").hasAnyAuthority(STUDENT, TEACHER)
             .antMatchers(HttpMethod.GET, "/document/{document-id}").hasAnyAuthority(STUDENT, TEACHER)
+
+            // LIBRARY
+            .antMatchers(HttpMethod.GET, "/library/student").hasAnyAuthority(STUDENT)
+            .antMatchers(HttpMethod.GET, "/library/teacher").hasAnyAuthority(TEACHER)
+            .antMatchers(HttpMethod.PATCH, "/library/{library-document-id}/access-right").hasAnyAuthority(STUDENT, TEACHER)
+
             // MAJOR
             .antMatchers(HttpMethod.GET, "/major").hasAnyAuthority(STUDENT, TEACHER)
             .antMatchers(HttpMethod.POST, "/major").hasAuthority(TEACHER)
             .antMatchers(HttpMethod.DELETE, "/major").hasAuthority(TEACHER)
+
             // FEEDBACK
             .antMatchers(HttpMethod.POST, "/feedback").hasAnyAuthority(TEACHER)
             .antMatchers(HttpMethod.PATCH, "/feedback").hasAnyAuthority(TEACHER)
@@ -84,7 +85,7 @@ internal class SecurityConfig(
             .anyRequest().authenticated()
 
         http
-            .apply(kr.hs.entrydsm.satellite.global.config.FilterConfig(jwtParser, objectMapper))
+            .apply(FilterConfig(jwtParser, objectMapper))
 
         return http.build()
     }
