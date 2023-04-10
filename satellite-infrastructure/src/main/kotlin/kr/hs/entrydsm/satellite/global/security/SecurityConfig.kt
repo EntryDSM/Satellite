4d1965fb0ad433cd02208ae08b99b248586abf29
rlaisqls <rlaisqls@gmail.com>
2023-04-10 09:36:24 +0900

    chore :: security logging disable

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityConfig.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityConfig.kt
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
-@EnableWebSecurity(debug = true)
 @Configuration
 internal class SecurityConfig(
     private val jwtParser: JwtParser,
