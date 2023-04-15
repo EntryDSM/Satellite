e8d44a4d5e990387b1ce497d246819e818c1be43
rlaisqls <rlaisqls@gmail.com>
2023-04-15 22:51:50 +0900

    create :: redisConfig

---
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/datasource/RedisConfig.kt
@@ -0,0 +1,33 @@
+package kr.hs.entrydsm.satellite.global.config.datasource
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Primary
+    @Bean
+    fun redisOperations(factory: ReactiveRedisConnectionFactory, objectMapper: ObjectMapper): ReactiveRedisOperations<String, Any> {
+        val jsonSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
+        val stringSerializer = StringRedisSerializer()
+
+        val serializationContext = RedisSerializationContext
+            .newSerializationContext<String, Any>()
+            .key(stringSerializer)
+            .value(jsonSerializer)
+            .hashKey(stringSerializer)
+            .hashValue(stringSerializer)
+            .build()
+
+        return ReactiveRedisTemplate(factory, serializationContext)
+    }
+}
