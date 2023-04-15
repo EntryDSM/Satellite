7510709313ed2f38c6c1f6658627355770d2b41a
rlaisqls <rlaisqls@gmail.com>
2023-04-15 22:50:35 +0900

    feat :: apply webClient

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/thirdparty/oauth/GoogleEmail.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/thirdparty/oauth/GoogleEmail.kt
@@ -1,16 +1,32 @@
 package kr.hs.entrydsm.satellite.global.thirdparty.oauth
 
+import kr.hs.entrydsm.satellite.global.exception.ForbiddenException
+import kr.hs.entrydsm.satellite.global.exception.InternalServerError
 import kr.hs.entrydsm.satellite.global.thirdparty.oauth.dto.response.GoogleEmailResponse
-import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 
-@FeignClient(name = "googleUserInfoFeign", url = "https://www.googleapis.com/oauth2/v1/userinfo")
-interface GoogleEmail {
-
-    @GetMapping
+@Component
+class GoogleEmail(
+    private val webClient: WebClient
+) {
     fun getEmail(
-        @RequestParam("access_token") accessToken: String,
-        @RequestParam("alt") alt: String = "json"
-    ): GoogleEmailResponse
+        accessToken: String,
+        alt: String = "json"
+    ) = webClient.post()
+        .uri {
+            it.path("https://www.googleapis.com/oauth2/v1/userinfo")
+                .queryParam("access_token", accessToken)
+                .queryParam("alt", alt)
+                .build()
+        }.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .retrieve()
+        .onStatus(HttpStatus::is4xxClientError) { Mono.error(ForbiddenException) }
+        .bodyToMono(object : ParameterizedTypeReference<GoogleEmailResponse>() {})
+        .onErrorMap { throw it }
+        .block() ?: throw InternalServerError
 }
