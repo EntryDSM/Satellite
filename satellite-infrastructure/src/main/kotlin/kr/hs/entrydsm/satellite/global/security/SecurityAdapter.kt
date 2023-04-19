7bb8f5fc2ce7ebed4d315cddc3151d73631bdf01
rlaisqls <rlaisqls@gmail.com>
2023-04-19 19:30:14 +0900

    update :: reactiveSecurityContextHolder

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityAdapter.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/SecurityAdapter.kt
@@ -1,29 +1,29 @@
 package kr.hs.entrydsm.satellite.global.security
 
+import kotlinx.coroutines.reactor.awaitSingle
 import kr.hs.entrydsm.satellite.common.annotation.Adapter
 import kr.hs.entrydsm.satellite.domain.auth.domain.Authority
 import kr.hs.entrydsm.satellite.domain.auth.spi.SecurityPort
-import kr.hs.entrydsm.satellite.domain.student.domain.Student
 import kr.hs.entrydsm.satellite.global.security.auth.details.StudentDetails
-import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
-import java.util.UUID
+import java.util.*
 
 @Adapter
 class SecurityAdapter(
     private val passwordEncoder: PasswordEncoder
 ) : SecurityPort {
 
-    override fun getCurrentStudent() =
-        (SecurityContextHolder.getContext().authentication.principal as StudentDetails).student
+    override suspend fun getCurrentStudent() =
+        (ReactiveSecurityContextHolder.getContext().awaitSingle().authentication.principal as StudentDetails).student
 
-    override fun getCurrentUserAuthority(): Authority {
-        val authorities = SecurityContextHolder.getContext().authentication.authorities
+    override suspend fun getCurrentUserAuthority(): Authority {
+        val authorities = ReactiveSecurityContextHolder.getContext().awaitSingle().authentication.authorities
         return Authority.valueOf(authorities.toList()[0].authority)
     }
 
-    override fun getCurrentUserId(): UUID {
-        return UUID.fromString(SecurityContextHolder.getContext().authentication.name)
+    override suspend fun getCurrentUserId(): UUID {
+        return UUID.fromString(ReactiveSecurityContextHolder.getContext().awaitSingle().authentication.name)
     }
 
     override fun encyptMatches(rawString: String, encryptedString: String) =
