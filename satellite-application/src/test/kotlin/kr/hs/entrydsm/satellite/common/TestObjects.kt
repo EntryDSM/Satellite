eb8bb1c8838899a69ec163a1e51f01b80e8fea39
rlaisqls <rlaisqls@gmail.com>
2023-04-19 19:58:40 +0900

    test :: domain

--- satellite-application/src/test/kotlin/kr/hs/entrydsm/satellite/common/TestObjects.kt
+++ satellite-application/src/test/kotlin/kr/hs/entrydsm/satellite/common/TestObjects.kt
@@ -4,13 +4,13 @@ import kr.hs.entrydsm.satellite.common.AnyValueObjectGenerator.anyValueObject
 import kr.hs.entrydsm.satellite.domain.document.domain.DocumentDomain
 import kr.hs.entrydsm.satellite.domain.document.domain.DocumentStatus
 import kr.hs.entrydsm.satellite.domain.document.domain.element.WriterInfoElement
-import kr.hs.entrydsm.satellite.domain.major.domain.Major
-import kr.hs.entrydsm.satellite.domain.student.domain.Student
+import kr.hs.entrydsm.satellite.domain.major.domain.MajorDomain
+import kr.hs.entrydsm.satellite.domain.student.domain.StudentDomain
 import java.util.*
 
 fun getTestDocument(
-    student: Student = anyValueObject(),
-    major: Major = anyValueObject(),
+    student: StudentDomain = anyValueObject(),
+    major: MajorDomain = anyValueObject(),
     status: DocumentStatus = DocumentStatus.CREATED,
     year: Int = 2023
 ) = DocumentDomain(
