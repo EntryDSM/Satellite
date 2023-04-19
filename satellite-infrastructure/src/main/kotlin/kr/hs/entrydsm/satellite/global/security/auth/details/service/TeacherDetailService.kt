6b54d2927c84c29322f77019f96a3ef3a99e08c1
rlaisqls <rlaisqls@gmail.com>
2023-04-19 22:38:49 +0900

    chore :: not null

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/auth/details/service/TeacherDetailService.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/security/auth/details/service/TeacherDetailService.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.reactor.mono
 import kr.hs.entrydsm.satellite.domain.teacher.spi.TeacherPort
 import kr.hs.entrydsm.satellite.global.exception.InvalidTokenException
 import kr.hs.entrydsm.satellite.global.security.auth.details.TeacherDetails
-import org.springframework.context.annotation.Primary
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
@@ -16,7 +15,7 @@ class TeacherDetailService(
     private val teacherPort: TeacherPort
 ): ReactiveUserDetailsService {
 
-    override fun findByUsername(username: String?): Mono<UserDetails>? = mono {
+    override fun findByUsername(username: String?): Mono<UserDetails> = mono {
         val teacher = teacherPort.queryById(UUID.fromString(username)) ?: throw InvalidTokenException
         return@mono TeacherDetails(teacher)
     }
