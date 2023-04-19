7146923076862603efa7cc95abfb647115dda30f
rlaisqls <rlaisqls@gmail.com>
2023-04-19 19:29:31 +0900

    update :: json Projperty

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/thirdparty/oauth/dto/response/GoogleAccessTokenResponse.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/thirdparty/oauth/dto/response/GoogleAccessTokenResponse.kt
@@ -1,5 +1,8 @@
 package kr.hs.entrydsm.satellite.global.thirdparty.oauth.dto.response
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class GoogleAccessTokenResponse(
+    @JsonProperty("access_token")
     val accessToken: String,
 )
