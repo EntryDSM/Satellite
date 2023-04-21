4401bdc4f36c8b31464110aa5d454363404858d4
rlaisqls <rlaisqls@gmail.com>
2023-04-22 01:47:25 +0900

    update :: config

---
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/CorsConfig.kt
@@ -0,0 +1,24 @@
+package kr.hs.entrydsm.satellite.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    fun corsFilter(): CorsWebFilter {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOriginPattern("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**",config)
+        return CorsWebFilter(source)
+    }
+}
--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/WebFluxConfig.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/WebFluxConfig.kt
@@ -12,7 +12,7 @@ import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 @EnableWebFlux
-class WebFluxConfiguration(
+class WebFluxConfig(
     private val objectMapper: ObjectMapper
 ) : WebFluxConfigurer {
 
