637035826c1460cb9364e7519f6cceee9514e0ce
rlaisqls <rlaisqls@gmail.com>
2023-05-29 16:19:42 +0900

    feat: binaryConverter

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/datasource/MongoDBConfig.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/config/datasource/MongoDBConfig.kt
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.satellite.global.config.datasource
 
+import com.fasterxml.uuid.impl.UUIDUtil
 import com.mongodb.MongoClientSettings
 import com.mongodb.MongoCredential
 import com.mongodb.ServerAddress
@@ -7,12 +8,20 @@ import com.mongodb.connection.ClusterSettings
 import com.mongodb.reactivestreams.client.MongoClient
 import com.mongodb.reactivestreams.client.MongoClients
 import org.bson.UuidRepresentation
+import org.bson.types.Binary
 import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.data.convert.ReadingConverter
+import org.springframework.data.convert.WritingConverter
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import java.util.*
+
 
 @Configuration
 @EnableReactiveMongoRepositories(basePackages = ["kr.hs.entrydsm.satellite"], reactiveMongoTemplateRef = "blogMongoTemplate")
@@ -23,7 +32,33 @@ class MongoDBConfig(
     @Bean
     fun blogMongoTemplate(mongoClient: MongoClient): ReactiveMongoTemplate {
         val factory = SimpleReactiveMongoDatabaseFactory(mongoClient, mongoProperties.database)
-        return ReactiveMongoTemplate(factory)
+        return ReactiveMongoTemplate(factory).apply {
+            (converter as MappingMongoConverter).apply {
+                customConversions = customConversions()
+                afterPropertiesSet()
+            }
+        }
+    }
+
+    @Bean
+    fun customConversions(): MongoCustomConversions {
+        val converters = listOf(
+            BinaryUUIDConverter(),
+            UUIDBinaryConverter()
+        )
+        return MongoCustomConversions(converters)
+    }
+
+    @ReadingConverter
+    class BinaryUUIDConverter : Converter<Binary, UUID> {
+        override fun convert(binary: Binary) =
+            UUIDUtil.uuid(binary.data)
+    }
+
+    @WritingConverter
+    class UUIDBinaryConverter : Converter<UUID, Binary> {
+        override fun convert(uuid: UUID) =
+            Binary(UUIDUtil.asByteArray(uuid))
     }
 
     @Bean
