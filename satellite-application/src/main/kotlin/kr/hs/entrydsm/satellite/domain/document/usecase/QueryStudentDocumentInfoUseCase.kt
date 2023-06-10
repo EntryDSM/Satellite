f38d85162799ffafd7d54f2af281e363f1ef3c9e
rlaisqls <rlaisqls@gmail.com>
2023-06-10 14:23:15 +0900

    chore: 학생 조회 삭제

--- satellite-application/src/main/kotlin/kr/hs/entrydsm/satellite/domain/document/usecase/QueryStudentDocumentInfoUseCase.kt
+++ satellite-application/src/main/kotlin/kr/hs/entrydsm/satellite/domain/document/usecase/QueryStudentDocumentInfoUseCase.kt
@@ -5,20 +5,16 @@ import kr.hs.entrydsm.satellite.domain.document.dto.DocumentInfoResponse
 import kr.hs.entrydsm.satellite.domain.document.exception.DocumentNotFoundException
 import kr.hs.entrydsm.satellite.domain.document.spi.DocumentPort
 import kr.hs.entrydsm.satellite.domain.feedback.spi.FeedbackPort
-import kr.hs.entrydsm.satellite.domain.student.exception.StudentNotFoundException
-import kr.hs.entrydsm.satellite.domain.student.spi.StudentPort
 import java.util.*
 
 @ReadOnlyUseCase
 class QueryStudentDocumentInfoUseCase(
-    private val studentPort: StudentPort,
     private val documentPort: DocumentPort,
     private val feedbackPort: FeedbackPort
 ) {
     suspend fun execute(studentId: UUID): DocumentInfoResponse {
 
-        val student = studentPort.queryById(studentId) ?: throw StudentNotFoundException
-        val document = documentPort.queryByWriterStudentId(student.id) ?: throw DocumentNotFoundException
+        val document = documentPort.queryByWriterStudentId(studentId) ?: throw DocumentNotFoundException
 
         val feedbackMap = feedbackPort.queryByDocumentId(document.id)
             .associate { it.elementId to it.comment }
