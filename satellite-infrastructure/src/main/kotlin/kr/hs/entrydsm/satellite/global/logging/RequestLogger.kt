ce0f6ab3d135d1bf7925687130203bea3c7195c8
rlaisqls <rlaisqls@gmail.com>
2023-06-22 14:46:02 +0900

    feat: log

--- satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/logging/RequestLogger.kt
+++ satellite-infrastructure/src/main/kotlin/kr/hs/entrydsm/satellite/global/logging/RequestLogger.kt
@@ -1,9 +1,9 @@
 package kr.hs.entrydsm.satellite.global.logging
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.reactor.mono
+import org.springframework.core.io.buffer.DataBufferUtils
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.http.server.reactive.ServerHttpResponse
 import org.springframework.stereotype.Component
@@ -28,37 +28,42 @@ class RequestLogger(
         if (exchange.request.headers["Content-Type"]?.contains("multipart/form-data") == true) {
             return@mono chain.filter(exchange).awaitSingleOrNull()
         } else {
-            logRequest(exchange.request, exchange.response)
+            logRequest(exchange.request,exchange.response)
         }
 
         return@mono chain.filter(exchange).awaitSingleOrNull()
     }
 
     @Throws(IOException::class)
-    private suspend fun logRequest(request: ServerHttpRequest, response: ServerHttpResponse) {
-
-        val requestTime = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
-        val requestIP = request.getHeader("X-Forwarded-For")?:"127.0.0.1"
-        val method = request.method?.name
-        val url = request.uri.toURL().path
-        val params = request.queryParams.toString()
-        val statusCode = response.statusCode?.value()
-        val body = request.securedBody()
-
-        // 2021-05-18 15:51:24.102 :: 127.0.0.1 [POST] (/user/auth?asd=asd 200) {asd: asd}
-        val log = String.format("$requestTime :: $requestIP [$method] ($url$params $statusCode) $body")
-        logWriter.writeLog(log)
+    private suspend fun logRequest(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse
+    ) {
+        DataBufferUtils.join(request.body)
+            .map { dataBuffer ->
+                val bytes = ByteArray(dataBuffer.readableByteCount())
+                dataBuffer.read(bytes)
+                DataBufferUtils.release(dataBuffer)
+                bytes
+            }.map {
+                // 2023-06-22 15:51:24.102 :: 127.0.0.1 [POST] 200 (/example/auth?asd=asd) {key: value}
+                val requestTime = LocalDateTime.now(ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+                val requestIP = request.getHeader("X-Forwarded-For") ?: "127.0.0.1"
+                val statusCode = response.statusCode?.value()
+                return@map String.format("$requestTime :: $requestIP [${request.method}] $statusCode /${request.path}?${request.uri.rawQuery ?: ""} ${securedBody(it)}")
+            }.map { log ->
+                logWriter.writeLog(log)
+            }.subscribe()
     }
 
-    private suspend fun ServerHttpRequest.securedBody(): String {
-        val bodyMap = objectMapper.readValue(
-            String(body.awaitSingle().asByteBuffer().array()), MutableMap::class.java
-        ) as MutableMap<String, Any>
 
-        bodyMap["password"]?.let {
-            bodyMap["password"] = "***"
+    private fun securedBody(body: ByteArray): String? {
+        val bodyMap = (objectMapper.readValue(String(body),MutableMap::class.java) as MutableMap<String,Any>)
+        "password".let {
+            if (bodyMap[it] != null) {
+                bodyMap[it] = "***"
+            }
         }
-
         return objectMapper.writeValueAsString(bodyMap)
     }
 
